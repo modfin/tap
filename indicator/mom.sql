173440dc28778f9d216d85804e511b6bf6600518
Rasmus Holm <rasmus.holm@modularfinance.se>
2018-11-20 18:44:27 +0100

    adding mom, roc and rocr

---
+++ indicator/mom.sql
@@ -0,0 +1,37 @@
+
+
+-- Momentum
+CREATE OR REPLACE FUNCTION tap_mom(_interval INT, _instrument_id BIGINT, _from DATE, _to DATE)
+  RETURNS TABLE("close_date" DATE, "mom" DOUBLE PRECISION)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+BEGIN
+
+  RETURN QUERY
+  WITH prices AS (
+      SELECT *
+      FROM tap_price_data_interval(_instrument_id, _from - 2*_interval, _to)
+  )
+  SELECT
+         d.close_date,
+         d.mom
+  FROM (
+       SELECT
+              today.close_date,
+              today.close_price - yesterday.close_price AS mom
+       FROM prices today
+              INNER JOIN
+                LATERAL (
+                       SELECT yesterday.close_price, yesterday.close_date
+                       FROM prices yesterday
+                       WHERE yesterday.close_date <= today.close_date
+                       ORDER BY yesterday.close_date DESC
+                       OFFSET _interval
+                       LIMIT 1
+                    ) yesterday ON TRUE
+       WHERE today.close_date BETWEEN _from AND _to
+       ) d
+  ;
+END;
+$$;
---
+++ indicator/roc.sql
@@ -0,0 +1,37 @@
+
+
+-- Rate of Change
+CREATE OR REPLACE FUNCTION tap_roc(_interval INT, _instrument_id BIGINT, _from DATE, _to DATE)
+  RETURNS TABLE("close_date" DATE, "roc" DOUBLE PRECISION)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+BEGIN
+
+  RETURN QUERY
+  WITH prices AS (
+      SELECT *
+      FROM tap_price_data_interval(_instrument_id, _from - 2*_interval, _to)
+  )
+  SELECT
+         d.close_date,
+         d.roc
+  FROM (
+       SELECT
+              today.close_date,
+              (today.close_price - yesterday.close_price) / yesterday.close_price AS roc
+       FROM prices today
+              INNER JOIN
+                LATERAL (
+                       SELECT yesterday.close_price, yesterday.close_date
+                       FROM prices yesterday
+                       WHERE yesterday.close_date <= today.close_date
+                       ORDER BY yesterday.close_date DESC
+                       OFFSET _interval
+                       LIMIT 1
+                    ) yesterday ON TRUE
+       WHERE today.close_date BETWEEN _from AND _to
+       ) d
+  ;
+END;
+$$;
